826202c2e1c2e4c2b5189b085c8ae7e92697f748
babywhale321 <107947235+babywhale321@users.noreply.github.com>
2023-01-14 23:41:14 -0500

    Add files via upload

---
+++ htpasswd-script.sh
@@ -0,0 +1,19 @@
+read -p "This program installs apache2-urils and will ask for a user and password for /etc/nginx/.htpasswd"
+read -p "Press enter to continue"
+
+if ! command -v apache2-utils &> /dev/null
+then
+    sudo apt install apache2-utils
+fi
+    echo "apache2-utils have already been installed."
+
+
+echo -e "Whats the user?"
+read uservar
+
+echo -e "Whats the password?"
+read passwdvar
+
+sudo sh -c "echo -n '$uservar:' >> /etc/nginx/.htpasswd"
+
+sudo sh -c "openssl passwd $passwdvar >> /etc/nginx/.htpasswd"
